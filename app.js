56173d7ff76786e327cc57bdf6b584c572472479
Andrew McMechan <coderwurst@gmail.com>
2020-02-02 21:17:22 +0100

    enable hotreload and add basic express endpoint

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const http = require("http");
+const express = require('express');
 
-const hostname = "127.0.0.1";
-const port = 3000;
+const app = express();
+const port = 8080;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("der Termin - Meeting, German");
+// TODO: allow cors for local development to be revisited before deployment
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/', (req, res, next) => {
+  console.log(`get recieved from ${req.url}`);
+  res.send('Hello from termin8!');
+  next();
 });
+
+app.listen(port, () => console.log(`termin8 listening on port ${port}!`));
