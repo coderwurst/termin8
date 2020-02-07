1e9d091d596992154433ed9b58efcf65f6cc4609
Andrew McMechan <coderwurst@gmail.com>
2020-02-07 06:36:27 +0100

    setup connection to atlas cluster

---
+++ dev-data/import-dev-data.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Meeting = require('../models/meetingModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful'));
+
+// read data and parse into an array of JS objects
+const meetings = JSON.parse(
+  fs.readFileSync(`${__dirname}/meetings.json`, 'utf8')
+);
+
+// import to DB
+const importData = async () => {
+  try {
+    await Meeting.create(meetings);
+    console.log('data successfully loaded');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+// delete from DB
+const deleteData = async () => {
+  try {
+    await Meeting.deleteMany();
+    console.log('data successfully deleted');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+// node dev-data/import-dev-data.js --import
+// node dev-data/import-dev-data.js --delete
