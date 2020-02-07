d59b19178f9721a0de96872333a9c8a274b163ca
Andrew McMechan <coderwurst@gmail.com>
2020-02-07 05:57:46 +0100

    connect to server with mongoose

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', error => {
+  console.log(`ERROR: ${error.name}, ${error.message}, ${error.stack}`);
+  console.log('Uncaught Exception Error! Shutting down...');
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('DB connection successful');
+  });
+
 const app = require('./app');
 
 const port = 8080;
