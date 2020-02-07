4e4fb72e0b22b007c66ec19e64e5d92fd31bdad3
Andrew McMechan <coderwurst@gmail.com>
2020-02-07 05:57:30 +0100

    setup basic meeting model

--- models/meetingModel.js
+++ models/meetingModel.js
@@ -1,4 +1,40 @@
-// time
-// place
-// user organiser
-// user(s) participants
+const mongoose = require('mongoose');
+
+const meetingSchema = new mongoose.Schema({
+  organiser: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'a meeting must have an organiser']
+  },
+  participants: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User'
+    }
+  ],
+  location: {
+    type: String,
+    required: [true, 'meeting must have a location']
+  },
+  time: {
+    type: Date,
+    required: [true, 'meeting must specify a time']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+meetingSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: 'organiser',
+    select: 'name'
+  });
+
+  next();
+});
+
+const Meeting = new mongoose.model('Meeting', meetingSchema);
+
+module.exports = Meeting;
