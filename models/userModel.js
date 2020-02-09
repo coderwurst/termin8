5b47e04f434c258bcc41f1e034a1d993ef49ee9f
Andrew McMechan <coderwurst@gmail.com>
2020-02-09 13:05:27 +0100

    initial user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,66 @@
-// name
-// email
-// avatar
-// role - private, business, group
-// password
-// passwordConfirm
-// passwordChangedAt
-// passwordResetToken
-// passwordResetExpires
-// active
-// calender
+const mongoose = require('mongoose');
+
+const emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'a user must have a name']
+  },
+  email: {
+    type: String,
+    unique: true,
+    validate: {
+      validator: function(element) {
+        return emailRegex.test(element);
+      },
+      message: props => `${props.element} is not a valid email!`
+    },
+    required: [true, 'a user must provide an email']
+  },
+  emailConfirmed: {
+    type: Boolean,
+    default: false
+  },
+  avatar: {
+    type: String,
+    default: 'default.jpg'
+  },
+  role: {
+    type: string,
+    enum: ['admin', 'private', 'business', 'group'],
+    default: 'private'
+  },
+  password: {
+    type: String,
+    required: [true, 'a user password is required'],
+    minLength: [8, 'a password must include 8 characters'],
+    select: false
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'A confiemed password is required'],
+    validate: {
+      validator: function(element) {
+        return element === this.password;
+      },
+      message: props => 'passwords do not match'
+    },
+    select: false
+  },
+  passwordChangetAt: Date,
+  passwordResetToken: String,
+  passwortResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false
+  },
+  calender: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Calender'
+  }
+});
+const User = new mongoose.model('User', userSchema);
+
+module.exports = User;
